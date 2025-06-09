f55e8b330a3f1972388734b0fa85400ab28e0135
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-06-10 01:37:50 +0600

    feat(CORS_ALLOWED_ORIGINS): added by localhost and live link

--- flower_seal/settings.py
+++ flower_seal/settings.py
@@ -22,6 +22,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://flower-sell.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 # Application definition
